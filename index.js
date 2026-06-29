64ee06ffbff0c3e0723d2f032333bc9c56b711cc
Carlos Téllez Sandoval <st4502@utr.edu.mx>
2026-06-29 12:59:52 -0600

    Conexión Mongo

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5100;
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "API running successfully"
+    });
+});
+
+const PORT = process.env.PORT || 5100;
 
 app.listen(PORT, () => {
-    console.log("Hello World");
+    console.log(`Server running on port ${PORT}`);
 });
